3b1957702479c87c4b472901c6ea37c0be2ed711
ericorenang <renan1999.rga@gmail.com>
2024-08-27 18:12:09 -0300

    Ajustes JS

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const textArea = document.querySelector(".text-area");
 const mensagem = document.querySelector(".mensagem");
-const imagem = document.getElementById('semMensagem'); // Seleciona a imagem
+const imagem = document.getElementById('semMensagem'); 
 
 function criptografar() {
-    imagem.style.display = "none"; // Esconde a imagem
+    imagem.style.display = "none"; 
 
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
     let stringEncriptada = document.getElementById('textoDescriptografado').value.toLowerCase();
@@ -21,7 +21,7 @@ function criptografar() {
 }
 
 function descriptografar() {
-    imagem.style.display = "none"; // Esconde a imagem
+    imagem.style.display = "none"; 
 
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
     let stringDescriptada = document.getElementById('textoDescriptografado').value.toLowerCase();
@@ -48,13 +48,13 @@ function copiar() {
 
     try {
         document.execCommand('copy');
-        mostrarImagem(); // Chama a função para mostrar a imagem novamente
+        mostrarImagem(); 
     } catch (err) {
         console.error('Falha ao copiar: ', err);
     }
 }
 
 function mostrarImagem() {
-    mensagem.textContent = ''; // Limpa a mensagem
-    imagem.style.display = block; // Mostra a imagem novamente
+    mensagem.textContent = ''; 
+    imagem.style.display = block; 
 }
